756e695d6a86de464e6836d535e26f43f74d803c
Maksimovich Vadim <vadim.maksimovich.1996@gmail.com>
2023-08-24 19:58:34 +0300

    Test

---
+++ test8.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import requests
+
+# Создаем заголовки
+header = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'pragma': 'no-cache',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+}
+
+# Функция для выполнения запроса и извлечения JSON
+
+
+def fetch_json(project):
+    if project == "ED":
+        url = f"https://edostavka.by/_next/data/W8J8cwtI4GSxWu3euE2du/category/5183.json?id=5183"
+    elif project == "EM":
+        url = f"https://emall.by/_next/data/1GzjMux1hWSC_vXtG809m/category/5183.json?id=5183"
+    else:
+        return None
+
+    session = requests.Session()
+    session.headers = header
+    response = session.get(url)
+
+    # Проверяем успешность запроса и наличие контента
+    if response.status_code == 200 and response.content:
+        try:
+            json_data = response.json()
+            return json_data
+        except ValueError:
+            return {"error": "Invalid JSON response"}
+    else:
+        error_info = {
+            "status_code": response.status_code,
+            "error_message": response.text
+        }
+        return {"error": error_info}
+
+
+def extract_h1(response):
+    try:
+        h1_value = response.get("pageProps", {}).get(
+            "page", {}).get("seo", {}).get("h1", "")
+        return h1_value
+    except Exception as e:
+        return f"Error: {e}"
+
+
+# Выводим информацию о датафрейме с результатами
+response = fetch_json('ED')
+h1 = extract_h1(response)
+print(h1)
